6a4ffa4e614f78c37303d7ee826abac38d946cd0
janisjee <jaanisjee@gmail.com>
2017-11-22 13:44:28 +0000

    test second commit

---
+++ Assets/testRepos.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRepos : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("testing GIT!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
